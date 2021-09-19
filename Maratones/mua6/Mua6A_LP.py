7855b1c3473ffae352aa978c3fbb2d73228ffa04
Luis Perez <le.perezl@uniandes.edu.co>
2021-09-19 15:11:19 -0500

    hahhh

--- Maratones/mua6/Mua6A_LP.py
+++ Maratones/mua6/Mua6A_LP.py
@@ -3,8 +3,9 @@ R="ring"
 S="star"
 UKN="unkown"
 
-pairs = []
+n = []
+e = []
 nodes, edges = map(int, input().split(" "))
-for i in range(nodes):
-    pairs.append(map(int, input().split(" ")))
-print(pairs)
+for i in range(edges):
+    a,b= map(int,input().split(" "))
+print(str(n+e) + " this is n: {n} and e: {e}")
